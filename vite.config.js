a323b30ea7ffd56b439295a52b8abbb9fc37fb02
Ghoudi Zakaria <zakariaghoudi1@gmail.com>
2026-03-30 21:35:58 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      manifest: {
-        name: '  صلاتي ',
-        short_name: 'Salati',
-        theme_color: '#1e3a8a',
-        icons: [
-          {
-            src: 'logo192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          }
-        ]
-      }
-    })
-  ]
+  plugins: [react()],
 })
-
