e798268faccf4268c32feeeee8c86c7485bdf04e
Ghoudi Zakaria <zakariaghoudi1@gmail.com>
2026-03-30 21:54:35 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc' // أضف -swc هنا
 
 export default defineConfig({
   plugins: [react()],
 })
+
